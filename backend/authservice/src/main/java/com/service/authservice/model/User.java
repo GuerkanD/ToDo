da2cd4ce5d55ee0a3905968fadf1676f3b2fc600
Guerkan <guerkan.deniz@hotmail.com>
2024-08-28 16:17:11 +0200

    feat: added Database Repository and Model

---
+++ backend/authservice/src/main/java/com/service/authservice/model/User.java
@@ -0,0 +1,86 @@
+package com.service.authservice.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "u_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(length = 30,nullable = false)
+    private String firstname;
+
+    @Column(length = 30,nullable = false)
+    private String lastname;
+
+    @Column(length = 64, unique = true)
+    private String email;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(Long id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- backend/authservice/src/main/java/com/service/authservice/service/AuthService.java
+++ backend/authservice/src/main/java/com/service/authservice/service/AuthService.java
@@ -1,5 +1,37 @@
 package com.service.authservice.service;
 
+import com.service.authservice.model.User;
+import com.service.authservice.payload.in.RegisterRequestDTO;
+import com.service.authservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(AuthService.class);
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean doesEmailExist(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean doesUserExistById(Long id) {
+        return userRepository.existsById(id);
+    }
+
+    public boolean createUser(RegisterRequestDTO registerRequestDTO) {
+        try {
+            userRepository.save(new User(registerRequestDTO.firstname(), registerRequestDTO.lastname(), registerRequestDTO.email(), registerRequestDTO.password()));
+            return true;
+        } catch (Exception e) {
+            logger.error("Error while registering user: {}", e.getMessage());
+            return false;
+        }
+    }
 }
